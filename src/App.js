a7e96021cfb6d582d854f5a414c2075318073525
Suning <sy2940@nyu.edu>
2021-07-22 17:16:58 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import {Canvas, useFrame} from 'react-three-fiber';
+import {useRef} from 'react';
+
+const Box = () => {
+  const ref = useRef()
+
+  useFrame(state => {
+    ref.current.rotation.x += 0.01
+    ref.current.rotation.y += 0.02
+  })
+
+  return (
+    <mesh ref={ref}>
+      <boxBufferGeometry />
+      <meshBasicMaterial color='blue'/>
+    </mesh>
+  )
+} 
+function App() {
+
+  return (
+    <Canvas style={{height:'100vh', width: '100vw'}}>
+      <Box />
+    </Canvas>
+  );
+}
+
+export default App;
