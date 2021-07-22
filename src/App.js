2f8887f8346dc284b2899c4a9856e635e3f2aa05
Suning <sy2940@nyu.edu>
2021-07-22 21:49:31 +0800

    ✨orbit helper and passing props to mesh

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import {Canvas, useFrame} from 'react-three-fiber';
+import {Canvas, useFrame, useThree, extend} from 'react-three-fiber';
 import {useRef} from 'react';
+import {
+  OrbitControls
+} from "three/examples/jsm/controls/OrbitControls";
+extend({OrbitControls})
 
-const Box = () => {
+const Orbit = () => {
+  const {camera, gl} = useThree()
+  return (
+    <orbitControls args={[camera, gl.domElement]}/>
+  )
+}
+
+const Box = (props) => {
   const ref = useRef()
 
   useFrame(state => {
@@ -10,8 +21,9 @@ const Box = () => {
   })
 
   return (
-    <mesh ref={ref}>
+    <mesh ref={ref} {...props}>
       <boxBufferGeometry />
+      <Orbit />
       <meshBasicMaterial color='blue'/>
     </mesh>
   )
@@ -19,8 +31,10 @@ const Box = () => {
 function App() {
 
   return (
-    <Canvas style={{height:'100vh', width: '100vw'}}>
-      <Box />
+    <Canvas style={{height:'100vh', width: '100vw', background: 'black'}}
+    camera={{position: [3, 3, 3]}}>
+      <Box position={[1,1,0]}/>
+      <axesHelper args={[5, ]}/>
     </Canvas>
   );
 }
